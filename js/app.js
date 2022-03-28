ea8d6c6f7c283a284f5da62195313498e7694094
Lin Kai <KLIN008@e.ntu.edu.sg>
2022-03-28 21:04:58 +0800

    Nothing wrong

--- js/app.js
+++ js/app.js
@@ -77,3 +77,27 @@ skillsHeader.forEach((el) => {
 // setTimeout(function() {
 //     loadBar.addClass('active');
 //   }, 500);
+
+
+// ============================ QUALIFICATION TABS =========================
+
+const tabs = document.querySelectorAll("[data-target]");
+const tabContents = document.querySelectorAll("[data-content");
+
+tabs.forEach(tab=>{
+    tab.addEventListener('click', ()=>{
+        const target = document.querySelector(tab.dataset.target);
+
+        tabContents.forEach(tabContent => {
+            tabContent.classList.remove('qualification__active');
+        })
+        target.classList.add('qualification__active');
+
+        tab.forEach(tab=>{
+            tab.classList.remove('qualification__active');
+    })
+
+        tab.classList.add("qualification__active")
+
+    })
+})
